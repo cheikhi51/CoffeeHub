af4e44322f6eb818b1aad2c79a5d8f5bca9a7373
Mohamed <cheikhimohamed51@gmail.com>
2025-06-16 19:57:31 +0100

    made some changes

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -3,6 +3,10 @@ function Contact(){
     return(
         <div className="contatct-container" id="contact">
             <h1 className="section-title">Contact Us</h1>
+            
+            <input/>
+            <input/>
+            <input/>
         </div>
     )
 
